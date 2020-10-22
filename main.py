e71afe26651525ca5f7908e094588119ea101b94
Vidya Raghavan Setlur <vsetlur@vsetlur-ltm.internal.salesforce.com>
2020-10-22 15:57:09 -0700

    updated to add translation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Main script
+### Main script
 # Language detection (some responses are in Spanish)
 # Translate above where possible.
 # Contraction cleanup / expansion (can't becomes can not)
@@ -7,10 +7,11 @@
 # Label Parts of speech (ideally the tag, but join back in the explanations)
 # Label prepositions and (if possible) identify all prepositional phrases)
 # Break out each word into its own row and number by order in sentence
-# If possible, label emotions - I was going down the EmoLex route.
+# If possible, label emotions.
 
 import fasttext
 from pycountry import languages
+from googletrans import Translator
 
 def detect_lang(sentences):
     PRETRAINED_MODEL_PATH = 'data/lid.176.bin'
@@ -24,13 +25,19 @@ def detect_lang(sentences):
             print(lang_code)
             lang_name = languages.get(alpha_2=lang_code).name
             print(lang_name)
+            # Translate the sentence if it is not English
+            if (lang_name != 'English'):
+                translate(sentence)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def translate(sentence):
+    translator = Translator()
+    translations = translator.translate(sentence, dest='en')
+    for translation in translations:
+        print(translation.origin, ' -> ', translation.text)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     detect_lang([['je mange de la nourriture'], ['Hoy es un buen día!'], ['the red fox sat in the moonlight.']])
+
 
